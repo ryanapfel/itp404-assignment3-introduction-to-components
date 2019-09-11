a0088e42419bd6fd9306f1b3908ea38ee9a6d53c
David Tang <david.tang@verizondigitalmedia.com>
2019-09-10 20:51:07 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MemberImage from './MemberImage';
+import GitHubRepoCard from './GitHubRepoCard';
+import Loading from './Loading';
+import { getMembers, getRepos } from './GitHubApi';
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      members: [],
+      repos: [],
+      loading: true
+    };
+  }
+  async componentDidMount() {
+    let [members, repos] = await Promise.all([
+      getMembers('emberjs'),
+      getRepos('emberjs')
+    ]);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.setState({ members, repos, loading: false });
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.state.members.length} Members of Ember.js</p>
+        <div>
+          {this.state.loading ? <Loading /> : this.state.members.map((member) => {
+            return <MemberImage member={member} key={member.id} />
+          })}
+        </div>
+        <div>
+        {this.state.loading ? <Loading /> : this.state.repos.map((repo) => {
+          return <GitHubRepoCard repo={repo} key={repo.id} />
+        })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
